dff7ae1fe8ce6757aa756428ad17a9e642fea870
onion <adgj5472@gmail.com>
2019-03-06 00:30:07 +0800

    add example

---
+++ examples/heroku/app.py
@@ -0,0 +1,49 @@
+import requests
+import re
+from flask import Flask, request, abort
+
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
+
+app = Flask(__name__)
+
+line_bot_api = LineBotApi('<Channel-access-token>')
+handler = WebhookHandler('<Channel-secret>')
+
+
+@app.route("/callback", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+
+    # get request body as text
+    body = request.get_data(as_text=True)
+    # print("body:",body)
+    app.logger.info("Request body: " + body)
+
+    # handle webhook body
+    try:
+        handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+@handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    print("event.reply_token:", event.reply_token)
+    print("event.message.text:", event.message.text)
+
+    if event.message.text == "!test":
+        content = '這是測試'
+        line_bot_api.reply_message(
+            event.reply_token,
+            TextSendMessage(text=content))
+        return 0
+
+if __name__ == '__main__':
+    app.run()
---
+++ tools/README.md
@@ -0,0 +1,3 @@
+# 工具使用方法
+## Ngrok  
+ngrok.exe http 5000  
